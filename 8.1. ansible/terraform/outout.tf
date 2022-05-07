770dc63e800b06fd2d1347c4c837ee445b9fa33e
nargamard <a.sarajkin@trs58.ru>
2022-05-07 20:39:46 +0300

    08-ansible-02-playbook

---
+++ 8.1. ansible/terraform/outout.tf
@@ -0,0 +1,7 @@
+output "external_ip_address_clickhouse-01_yandex_cloud" {
+  value = "${yandex_compute_instance.clickhouse-01.network_interface.0.nat_ip_address}"
+}
+
+output "external_ip_address_vector-01_yandex_cloud" {
+  value = "${yandex_compute_instance.vector-01.network_interface.0.nat_ip_address}"
+}
